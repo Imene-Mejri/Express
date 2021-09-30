a1a1782f7f916ef44dfdaccf5d2c76fe08a4b8c3
core.pager=cat <mejriimene501@gmail.com>
2021-09-30 11:13:56 +0300

    aupadate2

--- server.js
+++ server.js
@@ -7,19 +7,16 @@ const hour = date.getHours()
 
 
 //the middleware
-const middleware = (req, res, next) => {
-    next()
 
-}
-app.use((middleware) => {
-    if ((hour > 8) && (hour < 17) && (day < 6)) {
+app.use((req, res, next) => {
+    if ((hour>8) && (hour<01)&&(day<6) ) {
         res.sendFile(__dirname + '/public/home.html');
-
-      
+    
+        next();
     } else {
 
-        res.sendFile(__dirname + '/public/close.html')
-     
+        res.sendFile(__dirname+ '/public/close.html')
+        next();
     }
 });
 
@@ -28,7 +25,7 @@ app.use(express.static(__dirname, 'public'));
 
 // {Listen}   declare the port and the callbackfunction
 
-app.listen(3000, (error) => {
+app.listen(3000,(error)=>{
     if (error) console.log('serveur is not running')
-    else console.log('sever is running on port 3000')
+    else console.log ('sever is running on port 3000')
 })
