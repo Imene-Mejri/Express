2290af7b7649db30c274a19c08be6ebd157f803e
core.pager=cat <mejriimene501@gmail.com>
2021-09-30 16:34:28 +0300

    update3

--- server.js
+++ server.js
@@ -8,20 +8,29 @@ const hour = new Date().getHours();
 console.log(hour)
 const day = new Date().getDay();
 console.log(day)
+   
 
+const middlewrare=(req,res,next)=>{
+    console.log('text from middleware')
+    next()
+}
+app.use(middlewrare)
 //the middleware
-app.use((req, res, next) => {
+
+app.get('/',(req, res) => {
     if (day >= 1 && day <= 5 && hour >= 9 && hour < 17) {
+        res.sendFile(__dirname +'/public/home.html');
       next();
     } else {
        
-          res.sendFile(path.join(__dirname, 'public', 'close.html'))
+      res.sendFile(__dirname,+ 'public/close.html')
     
   
     }
   });
-
+  app.use(express.static(__dirname, 'public'))
 // {Listen}   declare the port and the callbackfunction
+
 
 app.listen(3000,()=>{
 console.log('the server is running')
